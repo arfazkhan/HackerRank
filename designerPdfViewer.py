0007901e36c0bdaf519aa6e37c4802dfb1391ef2
Arfaz Khan <arfuzkhan@gmail.com>
2023-10-25 01:30:36 +0530

    updated problem statement

--- designerPdfViewer.py
+++ designerPdfViewer.py
@@ -1,6 +1,3 @@
-#ProblemStatement:
-#https://www.hackerrank.com/challenges/designer-pdf-viewer/problem?isFullScreen=true
-
 def designerPdfViewer(h, word):
     max_height = max(h[ord(char) - ord('a')] for char in word)
     area = max_height * len(word)
