479712b3124e47d4a333e09d2eb47117947b18ba
Arfaz Khan <arfuzkhan@gmail.com>
2023-10-25 00:23:07 +0530

    update

--- BeautifulTriplets.py
+++ BeautifulTriplets.py
@@ -9,5 +9,3 @@ def beautifulTriplets(d, arr):
 
     return count
 
-#Logic:
-#I start by converting the array into a set for faster lookups. Then, I iterate over each element in the array. For each element, I check if the element incremented by 'd' and the element incremented by '2d' are both present in the set. If they are, it means I've found a beautiful triplet, so I increment my count. This approach allows me to find all beautiful triplets in the array with just a single pass, significantly improving the efficiency of the algorithm.
