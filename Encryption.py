51884c93b45218a906fb2b8fd1817d510e0005eb
Arfaz Khan <arfuzkhan@gmail.com>
2023-10-25 00:37:59 +0530

    update

--- Encryption.py
+++ Encryption.py
@@ -1,5 +1,3 @@
-
-#https://www.hackerrank.com/challenges/encryption
 import math
 
 def encryption(s):
@@ -15,5 +13,3 @@ def encryption(s):
 
     return result
 
-#Logic:
-#we're encrypting a message by arranging its characters into a grid and then reading the characters column by column. We remove spaces from the message and calculate the grid's dimensions based on the square root of the message length, visualizing it as a rectangular layout. We then construct the encrypted message by iterating through the columns, collecting characters from the original message and separating words with spaces. This method efficiently transforms the message into an encrypted format for secure communication.
